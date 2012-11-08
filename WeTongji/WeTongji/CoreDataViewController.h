3891e0ad4469623543acb6781258be7413775e0e
tzx7788 <tzx7788@me.com>
2012-11-08 14:07:22 +0800

    add CoreDataViewController

---
+++ WeTongji/WeTongji/CoreDataViewController.h
@@ -0,0 +1,15 @@
+//
+//  CoreDataViewController.h
+//  WeTongji
+//
+//  Created by tang zhixiong on 12-11-8.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreDataViewController : UIViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ WeTongji/WeTongji/CoreDataViewController.m
@@ -0,0 +1,55 @@
+//
+//  CoreDataViewController.m
+//  WeTongji
+//
+//  Created by tang zhixiong on 12-11-8.
+//
+//
+
+#import "CoreDataViewController.h"
+#import <CoreData/CoreData.h>
+
+@interface CoreDataViewController ()
+
+@end
+
+@implementation CoreDataViewController
+
+@synthesize managedObjectContext=_managedObjectContext;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)saveContext
+{
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
+}
+
+@end
