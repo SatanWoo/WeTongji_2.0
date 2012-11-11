62daea5cf4c94a30cad1361958609e721bb3e115
Ziqi Wu <Ziqi.Wu@bmw.com>
2012-11-11 23:09:00 +0800

    Ziqi:Add Page Control ViewController

---
+++ WeTongji/WeTongji/WUPageControlViewController.h
@@ -0,0 +1,13 @@
+//
+//  WUPageControlViewController.h
+//  WeTongji
+//
+//  Created by 吴 wuziqi on 12-11-11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WUPageControlViewController : UIViewController
+
+@end
---
+++ WeTongji/WeTongji/WUPageControlViewController.m
@@ -0,0 +1,43 @@
+//
+//  WUPageControlViewController.m
+//  WeTongji
+//
+//  Created by 吴 wuziqi on 12-11-11.
+//
+//
+
+#import "WUPageControlViewController.h"
+
+@interface WUPageControlViewController ()
+
+@end
+
+@implementation WUPageControlViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
