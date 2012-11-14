de633b598cc769ced87c3d833e29ff8ae8056da4
Ziqi Wu <Ziqi.Wu@bmw.com>
2012-11-14 23:32:06 +0800

    Ziqi:FolderView

---
+++ WeTongji/WeTongji/WUFolderView.h
@@ -0,0 +1,16 @@
+//
+//  WUFolderView.h
+//  WeTongji
+//
+//  Created by 吴 wuziqi on 12-11-14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WUFolderView : UIView
+
+@property (nonatomic ,weak) IBOutlet UIButton *tapButton;
+
+
+@end
