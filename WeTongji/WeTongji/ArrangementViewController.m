e015f7b1568c3c6e8e468a91b489a383271e75c0
tzx7788 <tzx7788@me.com>
2012-11-17 11:53:48 +0800

    animation of section view in arrangement is available

--- WeTongji/WeTongji/ArrangementViewController.m
+++ WeTongji/WeTongji/ArrangementViewController.m
@@ -12,6 +12,7 @@
 #import "WUArrangementSectionHeaderView.h"
 
 @interface ArrangementViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *sectionTableView;
 - (void)configureTableView;
 @end
 
@@ -36,6 +37,7 @@
 - (void)viewDidUnload
 {
     [self setArrangementTableView:nil];
+    [self setSectionTableView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -58,17 +60,19 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
+    if ( tableView == self.sectionTableView ) return 9;
     return 10;
 }
 
 - (float)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
 {
-    return 5;
+    if ( tableView == self.sectionTableView ) return 80;
+    return 0;
 }
 
 -(CGFloat)tableView:(UITableView*)tableView heightForFooterInSection:(NSInteger)section
 {
-    return 15.0;
+    return 0;
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
@@ -79,9 +83,20 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    ArrangementCell *cell = [tableView dequeueReusableCellWithIdentifier:kArrangementCell];
-    if (cell == nil) {
-        cell = [[ArrangementCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kArrangementCell];
+    UITableViewCell *cell;
+    if ( tableView == self.sectionTableView )
+    {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"transparentTableCell"];
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"transparentTableCell"];
+        }
+    }
+    else
+    {
+        cell = [tableView dequeueReusableCellWithIdentifier:kArrangementCell];
+        if (cell == nil) {
+            cell = [[ArrangementCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kArrangementCell];
+        }
     }
     return cell;
 }
@@ -90,4 +105,13 @@
 {
     return [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
 }
+
+-(void) scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    if ( scrollView == self.arrangementTableView )
+    {
+        [self.sectionTableView scrollRectToVisible:CGRectMake(self.arrangementTableView.contentOffset.x, self.arrangementTableView.contentOffset.y,self.arrangementTableView.bounds.size.width, self.arrangementTableView.bounds.size.height)  animated:NO];
+    }
+}
+
 @end
