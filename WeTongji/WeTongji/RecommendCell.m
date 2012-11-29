7f821ff6ef7c12f6cb735ea9af9324ebad9810b2
Ziqi Wu <Ziqi.Wu@bmw.com>
2012-11-29 19:51:45 +0800

    Ziqi:Fix issue 140

--- WeTongji/WeTongji/RecommendCell.m
+++ WeTongji/WeTongji/RecommendCell.m
@@ -9,6 +9,7 @@
 #import "RecommendCell.h"
 #import <WeTongjiSDK/WeTongjiSDK.h>
 #define kOffset 6
+#define kMaxWidth 167
 
 @implementation RecommendCell
 @synthesize infoTitle = _infoTitle;
@@ -30,16 +31,29 @@
 {
     self.infoTitle.text = information.title;
     [self.infoTitle sizeToFit];
+    
+    if (self.infoTitle.frame.size.width > kMaxWidth) {
+        CGRect oldFrame = self.infoTitle.frame;
+        oldFrame.size.width = kMaxWidth;
+        self.infoTitle.frame = oldFrame;
+        
+        CGRect iconFrame = self.icon.frame;
+        iconFrame.origin.x = iconFrame.origin.x - 2;
+        self.icon.frame = iconFrame;
+    } else {
+        CGRect oldFrame = self.icon.frame;
+        CGRect titleFrame = self.infoTitle.frame;
+        float newX = titleFrame.origin.x + kOffset + titleFrame.size.width;
+        oldFrame.origin.x = newX;
+        self.icon.frame = oldFrame;
+    }
+    
     self.source.text = information.source;
     self.count.text = [information.read stringValue];
     [self.preview setImageWithURL:[NSURL URLWithString: information.image]];
     _information = information;
     
-    CGRect oldFrame = self.icon.frame;
-    CGRect titleFrame = self.infoTitle.frame;
-    float newX = titleFrame.origin.x + kOffset + titleFrame.size.width;
-    oldFrame.origin.x = newX;
-    self.icon.frame = oldFrame;
+   
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
