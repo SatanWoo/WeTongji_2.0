45d9e36088d55c8bc48d1d117d40323a49a4aedd
Tang Zhixiong <tzx7788@me.com>
2012-12-02 22:38:21 +0800

    issue 129 fixed

--- WeTongji/WeTongji/WUArrangementSectionHeaderView.m
+++ WeTongji/WeTongji/WUArrangementSectionHeaderView.m
@@ -24,6 +24,15 @@
 
 -(void) setDate:(NSDate *)date
 {
+    NSDate * today = [NSDate dateWithTimeIntervalSinceNow: 60 * 60 * 8];
+    NSInteger interval = [today timeIntervalSince1970] / ( 60 * 60 * 24 );
+    today = [NSDate dateWithTimeIntervalSince1970:interval * 60 * 60 * 24];
+    if ( [date timeIntervalSinceDate:today] >= 0 &&
+        [date timeIntervalSinceDate:today] < 60 * 60 * 24 )
+    {
+#warning 这里调颜色
+        self.day.textColor = [UIColor orangeColor];
+    }
     NSDateFormatter *form = [[NSDateFormatter alloc] init];
     [form setDateFormat:@"dd"];
     self.day.text = [form stringFromDate:date];
