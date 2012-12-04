b1e8ec8b57b6feb327a48c29a9fa14d0a16150e9
Tang Zhixiong <tzx7788@me.com>
2012-12-04 22:21:33 +0800

    刷新时间颜色调整 60分钟内为橙色

---
+++ WeTongji/WeTongji/AutoResizeLabel.m
@@ -0,0 +1,27 @@
+//
+//  AutoResizeLabel.m
+//  WeTongji
+//
+//  Created by Tang Zhixiong on 12-12-4.
+//
+//
+
+#import "AutoResizeLabel.h"
+
+@implementation AutoResizeLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+-(void) setText:(NSString *)text
+{
+    [super setText:text];
+}
+
+@end
