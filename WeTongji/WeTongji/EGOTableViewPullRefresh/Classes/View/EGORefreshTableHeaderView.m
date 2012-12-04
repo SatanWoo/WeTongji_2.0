f81a95d1aac8c42733339da63ead117f7f4dca70
Tang Zhixiong <tzx7788@me.com>
2012-12-04 11:12:18 +0800

    issue 154 fixed

--- WeTongji/WeTongji/EGOTableViewPullRefresh/Classes/View/EGORefreshTableHeaderView.m
+++ WeTongji/WeTongji/EGOTableViewPullRefresh/Classes/View/EGORefreshTableHeaderView.m
@@ -112,7 +112,7 @@
 		NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
 		[formatter setAMSymbol:@"AM"];
 		[formatter setPMSymbol:@"PM"];
-		[formatter setDateFormat:@"MM/dd/yyyy hh:mm:a"];
+		[formatter setDateFormat:@"MM/dd/yyyy hh:mm"];
 		_lastUpdatedLabel.text = [NSString stringWithFormat:@"最近更新: %@", [formatter stringFromDate:date]];
 		[[NSUserDefaults standardUserDefaults] setObject:_lastUpdatedLabel.text forKey:@"EGORefreshTableView_LastRefresh"];
 		[[NSUserDefaults standardUserDefaults] synchronize];
